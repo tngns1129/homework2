dc76e06eade59c203b13f76993f464c4450262b4
suyeon <thdcjsryghl@naver.com>
2020-06-17 17:12:48 +0900

    devB

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -2,6 +2,6 @@
 public class A {
 	public static void main(String[] args) {
 		System.out.println("HI A");
-
+		System.out.println("bye AA");
 	}
 }
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -2,6 +2,6 @@
 public class  B{
 	public static void main(String[] args) {
 		System.out.println("HI B");
-
+		System.out.println("bye BB");
 	}
 }
